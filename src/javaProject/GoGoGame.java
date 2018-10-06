ad7216a56df820d5130afccba8e53305cf2e53b3
gil1122 <gil1122@naver.com>
2018-10-06 21:19:06 +0900

    메인화면과 클릭시 넘어가는 화면까지 구현

--- src/javaProject/GoGoGame.java
+++ src/javaProject/GoGoGame.java
@@ -1,5 +1,6 @@
 package javaProject;
 
+import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -7,45 +8,106 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 
 
 public class GoGoGame extends JFrame{
-	private Image introBackground=new ImageIcon(Main.class.getResource("../image/introBackground.png")).getImage();
+	private Image background=new ImageIcon(Main.class.getResource("../image/introBackground.png")).getImage();
 	private Image screenImage;
 	private Graphics screenGraphic;
 	
 	private ImageIcon startButtonImage=new ImageIcon(Main.class.getResource("../image/startButton.png"));
 	private JButton startButton=new JButton(startButtonImage);
+	private ImageIcon startButtonPressedImage=new ImageIcon(Main.class.getResource("../image/startButtonPressed.png"));
+	private ImageIcon descriptionButtonImage=new ImageIcon(Main.class.getResource("../image/descriptionButton.png"));
+	private JButton descriptionButton=new JButton(descriptionButtonImage);
+	private ImageIcon descriptionButtonPressedImage=new ImageIcon(Main.class.getResource("../image/descriptionButtonPressed.png"));
+	private ImageIcon exitButtonImage1=new ImageIcon(Main.class.getResource("../image/exitButton.png"));
+	private JButton exitButton1=new JButton(exitButtonImage1);
+	private ImageIcon exitButtonPressedImage1=new ImageIcon(Main.class.getResource("../image/exitButtonPressed.png"));
+	
+	private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../image/menuBar.jpg")));
+	private ImageIcon exitButtonEnteredImage = new ImageIcon(Main.class.getResource("../image/exitButtonEntered.png"));
+	private ImageIcon exitButtonBasicImage = new ImageIcon(Main.class.getResource("../image/exitButtonBasic.png"));
+	private JButton exitButton = new JButton(exitButtonBasicImage);
+	
+	private ImageIcon backButtonImage = new ImageIcon(Main.class.getResource("../image/backButton.png"));
+	private JButton backButton = new JButton(backButtonImage);
+
+	
+	private int mouseX, mouseY;
 	
 	public GoGoGame() {
-		//setUndecorated(true);
-		//setBackground(new Color(0, 0, 0, 0));
-		Toolkit kit=Toolkit.getDefaultToolkit();
-		Dimension screenSize=kit.getScreenSize();
-		setTitle("Go Go");
+		setUndecorated(true); // frame을 없애고
+		setBackground(new Color(0, 0, 0,0)); // 배경을 투명으로 설정했기 때문에 그래픽에서 배경이미지와 버튼같은 컴포넌트만 표현됨
+		//Toolkit kit=Toolkit.getDefaultToolkit();
+		//Dimension screenSize=kit.getScreenSize();
+		//setTitle("Go Go");
 		setSize(1080,715);
 		setResizable(false);
+		setLayout(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
-		setLayout(null);
-		setLocation(screenSize.width/8,screenSize.height/7);
+		setLocation(1080/8,715/7);
 		// Image img=kit.getImage("goIcon.png");
-		Image img=kit.createImage("../image/goIcon.png");
-		setIconImage(img);
-		introBackground=new ImageIcon(Main.class.getResource("../image/introBackground.png")).getImage();
+		//Image img=kit.createImage("../image/goIcon.png");
+		//setIconImage(img);
 		
+		exitButton.setBounds(1045, 0, 30, 30); // 메뉴바 exit버튼
+		exitButton.setBorderPainted(false);
+		exitButton.setContentAreaFilled(false);
+		exitButton.setFocusPainted(false);
+		exitButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				exitButton.setIcon(exitButtonEnteredImage);
+				exitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				exitButton.setIcon(exitButtonBasicImage);
+				exitButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Music buttonEnteredMusic = new Music("mouseClick.mp3", false);
+				buttonEnteredMusic.start();
+				System.exit(0);
+			}
+		});
+		add(exitButton);
 		
-		startButton.setBounds(100,180,300,100);
+		menuBar.setBounds(0, 0, 1280, 30); // 메뉴바 
+		menuBar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				mouseX = e.getX();
+				mouseY = e.getY();
+			}
+		});
+		menuBar.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				int x = e.getXOnScreen();
+				int y = e.getYOnScreen();
+				setLocation(x - mouseX, y - mouseY);
+			}
+		});
+		add(menuBar);
+		
+		startButton.setBounds(100,200,300,100); // 시작버튼
 		startButton.setBorderPainted(false);
 		startButton.setContentAreaFilled(false);
 		startButton.setFocusPainted(false);
 		startButton.addMouseListener(new MouseAdapter() {
 			public void mouseEntered(MouseEvent e) {
-				startButton.setIcon(startButtonImage);
+				startButton.setIcon(startButtonPressedImage);
 				startButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
 			}
 			public void mouseExited(MouseEvent e) {
@@ -55,25 +117,72 @@ public class GoGoGame extends JFrame{
 			public void mousePressed(MouseEvent e) {
 				Music ButtonEnteredMusic=new Music("mouseClick.mp3",false);
 				ButtonEnteredMusic.start();
-			}
+				descriptionButton.setVisible(false);
+				exitButton1.setVisible(false);
+				startButton.setVisible(false);
+				background=new ImageIcon(Main.class.getResource("../image/introBackgroundBlur.png")).getImage();
+			};
 		});
 		add(startButton);
 		
+		descriptionButton.setBounds(100,300,300,100); // 설명버튼
+		descriptionButton.setBorderPainted(false);
+		descriptionButton.setContentAreaFilled(false);
+		descriptionButton.setFocusPainted(false);
+		descriptionButton.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) {
+				descriptionButton.setIcon(descriptionButtonPressedImage);
+				descriptionButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			public void mouseExited(MouseEvent e) {
+				descriptionButton.setIcon(descriptionButtonImage);
+				descriptionButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			public void mousePressed(MouseEvent e) {
+				Music ButtonEnteredMusic=new Music("mouseClick.mp3",false);
+				ButtonEnteredMusic.start();
+				descriptionButton.setVisible(false);
+				exitButton1.setVisible(false);
+				startButton.setVisible(false);
+				background=new ImageIcon(Main.class.getResource("../image/introBackgroundBlur.png")).getImage();
+			}
+		});
+		add(descriptionButton);
 		
+		exitButton1.setBounds(100,400,300,100); // 종료버튼
+		exitButton1.setBorderPainted(false);
+		exitButton1.setContentAreaFilled(false);
+		exitButton1.setFocusPainted(false);
+		exitButton1.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) {
+				exitButton1.setIcon(exitButtonPressedImage1);
+				exitButton1.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+			public void mouseExited(MouseEvent e) {
+				exitButton1.setIcon(exitButtonImage1);
+				exitButton1.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			public void mousePressed(MouseEvent e) {
+				Music ButtonEnteredMusic=new Music("mouseClick.mp3",false);
+				ButtonEnteredMusic.start();
+				System.exit(0);
+			}
+		});
+		add(exitButton1);
 		Music introMusic=new Music("ARRIVAL.MP3",true);
 		introMusic.start(); // 스레드 실행시작
+		
+		
 	}
 	public void paint(Graphics g) { // // 이중 버퍼링기법(이미지 전환하면 화면 깜빡임이 없고 부드럽게 하기 위해서)
 		screenImage = createImage(1080, 715);
 		screenGraphic = screenImage.getGraphics();
 		screenDraw(screenGraphic);
 		g.drawImage(screenImage, 0, 0, null);
-		//screenGraphic.drawImage(background,0,0,null);
-		//g.drawImage(screenImage, 0, 0, null);
 	}
 
 	public void screenDraw(Graphics g) {
-		g.drawImage(introBackground, 0, 0, null);
+		g.drawImage(background, 0, 0, null);
 		paintComponents(g);
 		this.repaint();
 	}
